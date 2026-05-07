9541071dccb8c157c6930909366030b9401414d1
Ana Flavia <magalhaesana3005@gmail.com>
2026-05-07 14:32:58 -0300

    Revert "alterado"
    This reverts commit 4e065ee9734af82d7c67c633288debfeffbf99c3.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert("Boas vindas ao jogo do mundo secreto!");
-let numeroMaximo = 50;
+let numeroMaximo = 10;
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1 ); 
 console.log(numeroSecreto);
 let chute;
