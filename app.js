4e065ee9734af82d7c67c633288debfeffbf99c3
Ana Flavia <magalhaesana3005@gmail.com>
2026-05-07 14:18:25 -0300

    alterado

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert("Boas vindas ao jogo do mundo secreto!");
-let numeroMaximo = 10;
+let numeroMaximo = 50;
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1 ); 
 console.log(numeroSecreto);
 let chute;
