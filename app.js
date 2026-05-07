329952305dadd461f1647b8edb0dbfddc0b45086
Ana Flavia <magalhaesana3005@gmail.com>
2026-05-07 14:33:16 -0300

    chute apagdo

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ alert("Boas vindas ao jogo do mundo secreto!");
 let numeroMaximo = 10;
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1 ); 
 console.log(numeroSecreto);
-let chute;
 let tentativas = 1;
 
 // enquanto não for o número secreto
